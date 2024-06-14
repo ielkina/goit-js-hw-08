15a4630ebadb10cb1956244a0f9bf7ed05dad981
ielkina <ielkinairinka@gmail.com>
2024-06-14 22:41:36 +0300

    modified:src/js/01-gallery.js modified:src/js/02-video.js modified:src/js/03-feedback.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,6 +4,25 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-value = 15
+import { sum as fns, add } from './02-video';
+
+//больше пяти импортов
+import * as data from './02-video';
+
+const sum = 10;
+
+console.log(sum);
+console.log(fns(1, 2));
+console.log(add(2, 2));
+console.log(data);
+console.log(data.add(5, 5));
+
+import value from './02-video';
+import {add, sum} from './02-video'
 
 console.log(value);
+
+console.log(add(12,12));
+console.log(sum(20,30));
+
+
